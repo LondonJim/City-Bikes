a3b7dd0e7b46a25c3e35dc6032b18f4eeefd5559
James Sutherland <jrsutherland78@googlemail.com>
2019-02-20 21:16:08 +0000

    add .Bike instance variable broken? can be set

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -6,8 +6,25 @@ describe Bike do
     @bike = Bike.new
   end
 
-  it 'responds to #working?' do
-    expect(@bike).to respond_to(:working?)
+  describe '#report_broken' do
+
+    it 'returns true when executed' do
+      expect(@bike.report_broken).to eq(true)
+    end
+
+  end
+
+  describe '#broken' do
+
+    it 'returns nil if object not created' do
+      expect(@bike.broken?).to eq(nil)
+    end
+
+    it 'returns true if object is created' do
+      @bike.instance_variable_set(:@broken, true)
+      expect(@bike.broken?).to eq(true)
+    end
+
   end
 
 end
