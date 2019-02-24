82fdcdf8a02e7c235d611a519b271509b92d4fe1
James Sutherland <jrsutherland78@googlemail.com>
2019-02-24 20:42:44 +0000

    include BikeContainer module in .DockingStation, .Van, .Garage, remove duplicate code

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,12 +1,9 @@
 require_relative 'bike'
+require_relative 'bike_container'
 
 class Garage
 
-  CAPACITY = 20
-
-  def initialize
-    @bikes = []
-  end
+  include BikeContainer
 
   def fix_bike
     bike_release_fail if empty?
@@ -17,47 +14,6 @@ class Garage
       end
     end
     fail 'All bikes fixed'
-  end
-
-  def add_bike(bike)
-    fail 'Capacity full' if full?
-    @bikes << bike
-  end
-
-  def release_broken_bike
-    bike_release_fail if empty?
-    for i in 0...@bikes.count
-      if @bikes[i].broken
-        return @bikes.delete_at(i)
-      end
-    end
-    bike_release_fail
-  end
-
-  def release_bike
-    bike_release_fail if empty?
-    for i in 0...@bikes.count
-      unless @bikes[i].broken
-        return @bikes.delete_at(i)
-      end
-    end
-    bike_release_fail
-  end
-
-  private
-
-  attr_reader :bike
-
-  def full?
-    @bikes.count >= CAPACITY
-  end
-
-  def empty?
-    @bikes.empty?
-  end
-
-  def bike_release_fail
-    fail 'No bikes available'
   end
 
 end
--- lib/van.rb
+++ lib/van.rb
@@ -1,52 +1,8 @@
 require_relative 'bike'
+require_relative 'bike_container'
 
 class Van
 
-  CAPACITY = 20
-
-  def initialize
-    @bikes = []
-  end
-
-  def add_bike(bike)
-    fail 'Capacity full' if full?
-    @bikes << bike
-  end
-
-  def release_broken_bike
-    bike_release_fail if empty?
-    for i in 0...@bikes.count
-      if @bikes[i].broken
-        return @bikes.delete_at(i)
-      end
-    end
-    bike_release_fail
-  end
-
-  def release_bike
-    bike_release_fail if empty?
-    for i in 0...@bikes.count
-      unless @bikes[i].broken
-        return @bikes.delete_at(i)
-      end
-    end
-    bike_release_fail
-  end
-
-  private
-
-  attr_reader :bike
-
-  def full?
-    @bikes.count >= CAPACITY
-  end
-
-  def empty?
-    @bikes.empty?
-  end
-
-  def bike_release_fail
-    fail 'No bikes available'
-  end
+  include BikeContainer
 
 end
